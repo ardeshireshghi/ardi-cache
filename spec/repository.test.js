257db836b529720c40520f3aaa06c1b7e9a509cd
Ardeshir Eshghi <ardeshir.eshghi@fundingcircle.com>
2020-01-22 01:09:33 +0000

    Fix cache repository spec by mocking S3 API calls

--- spec/repository.test.js
+++ spec/repository.test.js
@@ -1,4 +1,6 @@
+import sinon from 'sinon';
 import { expect } from 'chai';
+
 import CacheRepository from '../src/repository';
 import S3Store from '../src/stores/s3';
 import ObjectStore from '../src/stores/object';
@@ -47,30 +49,44 @@ describe('#CacheRepository', () => {
     });
 
     describe('#S3Store', () => {
-      beforeEach((done) => {
-        repository = new CacheRepository(new S3Store(config.stores.s3.bucket));
-        repository.forget('foo').then(() => {
-          done();
-        })
+      let mockS3Connection;
+
+      beforeEach(() => {
+        mockS3Connection = {
+          putObject: sinon.stub(),
+          getObject: sinon.stub()
+        };
+
+
+        mockS3Connection.putObject.returns({
+          promise: () => Promise.resolve()
+        });
+
+        const s3Store = new S3Store(config.stores.s3.bucket, mockS3Connection);
+        repository = new CacheRepository(s3Store);
       });
 
-      it('Should return the cache key value with S3 store', (done) => {
-        repository.put('foo', 'bar')
-        .then(() => {
-          return repository.get('foo');
-        })
-        .then((value) => {
-          expect(value).to.equal('bar');
-          done();
+      it('Should return the cache key value with S3 store', async () => {
+        mockS3Connection.getObject.returns({
+          promise: () => Promise.resolve({
+            Body: 'bar'
+          })
         });
+
+        await repository.put('foo', 'bar');
+
+        expect(await repository.get('foo')).to.equal('bar');
       });
 
-      it('Should return null when cache key does not exist', (done) => {
-        repository.get('foo')
-        .then((value) => {
-          expect(value).to.equal(null);
-          done();
+      it('Should return null when cache key does not exist', async () => {
+        const error = new Error();
+        error.name = 'NoSuchKey';
+
+        mockS3Connection.getObject.returns({
+          promise: () => Promise.reject(error)
         });
+
+        expect(await repository.get('foo')).to.equal(null);
       });
     });
   });
